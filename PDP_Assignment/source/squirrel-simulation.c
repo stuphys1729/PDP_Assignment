6e4005d077933ee8f6399c015ab621a99333356e
stuphys1729 <s1432366@sms.ed.ac.uk>
2018-03-29 23:16:31 +0100

    More debug output

--- PDP_Assignment/source/squirrel-simulation.c
+++ PDP_Assignment/source/squirrel-simulation.c
@@ -152,6 +152,7 @@ static void coordinatorCode() {
 		printf("Coordinator started squirrel %d on MPI process %d\n", i, workerPid);
 	}
 
+
 	// Run the simulation
 	int current_month = 1, month_end = 0;
 	MPI_Status environment_statuses[num_env_cells];
@@ -159,6 +160,7 @@ static void coordinatorCode() {
 	int workerStatus = 1;
 	while (workerStatus) {
 		// Check if all cells have finished the current month
+		printf("Checking month\n");
 		MPI_Testall(num_env_cells, environment_requests[current_month], &month_end, environment_statuses);
 		if (month_end) {
 			printf("[%2.4f] | All cells have completed month %d | ", MPI_Wtime() - start_time, current_month);
@@ -181,6 +183,7 @@ static void coordinatorCode() {
 			error_msg("All the squirrels died :( ");
 		}
 		workerStatus = shouldWorkerStop();
+		printf("workerStatus: %d\n", workerStatus);
 	}
 	printf("Coordinator is finishing...\n");
 }
